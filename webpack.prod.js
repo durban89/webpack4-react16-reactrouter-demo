0339f14f92a699b387174c64a3301aa3132a14a8
durban.zhang <durban.zhang@gmail.com>
2018-06-12 18:34:49 +0800

    source map

--- webpack.prod.js
+++ webpack.prod.js
@@ -2,10 +2,12 @@ const path = require('path');
 const merge = require('webpack-merge');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const CleanWebpackPlugin = require('clean-webpack-plugin');
+const UglifyJsPlugin = require('uglifyjs-webpack-plugin');
 const common = require('./webpack.common');
 
 module.exports = merge(common, {
   mode: 'production',
+  devtool: 'source-map',
   entry: {
     app: [
       './src/index.jsx',
@@ -23,6 +25,9 @@ module.exports = merge(common, {
       title: 'React + ReactRouter',
       filename: './index.html', // 调用的文件
       template: './index.html', // 模板文件
+    }),
+    new UglifyJsPlugin({
+      sourceMap: true,
     }),
   ],
 });
