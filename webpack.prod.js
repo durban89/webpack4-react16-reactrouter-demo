4dca72b2ebd9d6d19e6f4918aa1802539e4d86ed
durban.zhang <durban.zhang@gmail.com>
2018-06-14 18:29:13 +0800

    manifest add and add vendor

--- webpack.prod.js
+++ webpack.prod.js
@@ -5,6 +5,7 @@ const HtmlWebpackPlugin = require('html-webpack-plugin');
 const CleanWebpackPlugin = require('clean-webpack-plugin');
 const UglifyJsPlugin = require('uglifyjs-webpack-plugin');
 const ExtractTextPlugin = require('extract-text-webpack-plugin');
+const ManifestPlugin = require('webpack-manifest-plugin');
 const common = require('./webpack.common');
 
 module.exports = merge(common, {
@@ -13,6 +14,11 @@ module.exports = merge(common, {
   entry: {
     app: [
       './src/index.jsx',
+    ],
+    vendor: [
+      'react',
+      'react-dom',
+      'redux',
     ],
   },
   output: {
@@ -37,5 +43,31 @@ module.exports = merge(common, {
     new ExtractTextPlugin({
       filename: 'main.[chunkhash].css',
     }),
+    new ManifestPlugin(),
+    new webpack.NamedModulesPlugin(),
   ],
+  optimization: {
+    splitChunks: {
+      chunks: 'initial', // 必须三选一： "initial" | "all"(默认就是all) | "async"
+      minSize: 0, // 最小尺寸，默认0
+      minChunks: 1, // 最小 chunk ，默认1
+      maxAsyncRequests: 1, // 最大异步请求数， 默认1
+      maxInitialRequests: 1, // 最大初始化请求书，默认1
+      name: () => {}, // 名称，此选项可接收 function
+      cacheGroups: { // 这里开始设置缓存的 chunks
+        priority: '0', // 缓存组优先级 false | object |
+        vendor: { // key 为entry中定义的 入口名称
+          chunks: 'initial', // 必须三选一： "initial" | "all" | "async"(默认就是异步)
+          test: /react|lodash|react-dom|redux/, // 正则规则验证，如果符合就提取 chunk
+          name: 'vendor', // 要缓存的 分隔出来的 chunk 名称
+          minSize: 0,
+          minChunks: 1,
+          enforce: true,
+          maxAsyncRequests: 1, // 最大异步请求数， 默认1
+          maxInitialRequests: 1, // 最大初始化请求书，默认1
+          reuseExistingChunk: true, // 可设置是否重用该chunk（查看源码没有发现默认值）
+        },
+      },
+    },
+  },
 });
